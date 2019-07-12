ec5d46bc141cebb2a9f431afe755476800153e68
Ryan McCarthy <ryanxmccarthy@gmail.com>
2019-07-11 21:36:42 -0700

    2 solved

--- javascript/2.js
+++ javascript/2.js
@@ -1,14 +1,21 @@
-let arr = [0, 1, 1, 2];
-let i = 4;
-let sum = 0;
+const sum = () => {
+  let previous = 0;
+  let current = 1;
+  let result = 0;
 
-while (arr[i-1] + arr[i-2] <= 4000000) {
-  arr[i] = arr[i-1] + arr[i-2];
-  i += 1;
+  for (let i = 0; ; i++) {
+    const next = previous + current;
+    if (next > 4000000) {
+      break;
+    }
+    if (next % 2 === 0) {
+      result += next;
+    }
+    previous = current;
+    current = next;
+  }
+
+  console.log(result);
 }
 
-for (var j in arr) {
-  sum += arr[j];
-}
-
-console.log(sum);
+sum();

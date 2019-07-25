ad13b180aa04f3cb14d83759eb7200977e2eea2d
Ryan McCarthy <ryanxmccarthy@gmail.com>
2019-07-24 21:38:35 -0700

    12 solved

--- javascript/12.js
+++ javascript/12.js
@@ -1,22 +1,23 @@
-var number=0;
-var n=1;
-while(number===0){
+var number = 0;
+var n = 1;
 
-var count=0;
-var trinum= (n*(n+1))/2;
+while (number === 0) {
+  var count = 0;
+  var trinum = (n * (n + 1)) / 2;
 
-for(i=1;i<=Math.sqrt(trinum);i++){
-if(trinum%i===0){             //finds multiples, if a multiple is found and it is less than the
-count+=2;                              //square root then there is a another one greater than the root
-}
+  for (i = 1; i <= Math.sqrt(trinum); i++) {
+    if (trinum % i === 0) {    
+      count += 2;                 
+    }
+  }
+
+  if (count > 500) {
+    number = trinum;
+    break;
+  }
+
+  n += 1;
+  console.log(count,trinum,n);
 }
 
-if(count>500){
-number=trinum;
-break;
-}
-
-n+=1;
-console.log(count,trinum,n);
-}
 console.log(number);
